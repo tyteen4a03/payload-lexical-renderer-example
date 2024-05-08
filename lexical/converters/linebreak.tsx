d406332ee58c48af1b71e4e224d36f252f786e5c
tyteen4a03 <tyteen4a03@thelastcode.io>
2024-05-08 07:56:45 +0200

    delete old code

--- lexical/converters/linebreak.tsx
+++
@@ -1,8 +0,0 @@
-import type { HTMLConverter } from "../types";
-
-export const LinebreakHTMLConverter: HTMLConverter<any> = {
-    converter() {
-        return <br />;
-    },
-    nodeTypes: ["linebreak"],
-};
